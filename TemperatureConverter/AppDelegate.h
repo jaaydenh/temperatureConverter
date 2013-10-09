aede155874881803656e9a11100ca5b7a45ad47a
Jaayden <jaayden.halko@gmail.com>
2013-10-08 20:59:23 -0700

    Initial Commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Jaayden on 10/8/13.
+//  Copyright (c) 2013 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
